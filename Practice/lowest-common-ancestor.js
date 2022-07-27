b7a1bc09965e0c37f5c69480b05a90ece118b09d
Opeyemi Jonah <60982863+opeyemi-jonah@users.noreply.github.com>
2022-07-27 07:32:43 -0600

    Create lowest-common-ancestor.js

---
+++ Practice/lowest-common-ancestor.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    const current = root;
+    
+    while (current){
+        if (p.val > current.val && q.val > current.val) current = current.right;
+        if (p.val < current.val && q.val < current.val) current = current.left;
+        else return current;
+    }
+    
+    
+};
