9c1336a49b8ef82f21d05f32d1ef8d86761ab266
Opeyemi Jonah <60982863+opeyemi-jonah@users.noreply.github.com>
2022-08-05 04:47:51 -0600

    Create BTMaxPath.js

---
+++ Practice/BTMaxPath.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxPathSum = function(root) {
+       const result = [root.val];
+    
+    function dfs(root){
+        
+        if (!root) return 0;
+        
+        let leftMax = dfs(root.left);
+        let rightMax = dfs(root.right);
+        
+        leftMax = Math.max(leftMax, 0);
+        rightMax = Math.max(rightMax, 0);
+        
+        //compute with spliting the tree
+        result[0] = Math.max(result[0],root.val + leftMax + rightMax);
+        
+        return root.val + Math.max(leftMax, rightMax);
+        
+    }
+    
+    dfs(root);
+    
+    return result[0];
+    
+    
+};
