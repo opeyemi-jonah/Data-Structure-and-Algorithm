d010f2820d7996a4af35f10fce4885206d7b4d00
Opeyemi Jonah <60982863+opeyemi-jonah@users.noreply.github.com>
2022-08-05 10:24:12 -0600

    Update containsDuplicate.js

--- Practice/containsDuplicate.js
+++ Practice/containsDuplicate.js
@@ -3,12 +3,14 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    
+    //Create a Set object,because Sets only allows unique values
     let newNum = new Set();
     
+    //add each element of the nums array to the set
     nums.forEach(num => newNum.add(num));
     
-    
+    //check if the size of the Set object is not the same as the nums array length
+    //if the size of te set and the length of the array are equal return false
     if (newNum.size !== nums.length) return true;
     else return false;
    
