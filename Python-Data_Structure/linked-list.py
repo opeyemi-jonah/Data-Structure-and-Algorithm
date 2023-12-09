aded6508ccd93eb64ba05d83b7944467c94df62d
opeyemi-jonah <gavrieljonah@gmail.com>
2023-12-08 17:45:03 -0700

    Added new folder

---
+++ Python-Data_Structure/linked-list.py
@@ -0,0 +1,7 @@
+#will learn about the implementation of a linked list in Python
+
+'''
+A linked list is a type of linear data structure similar to arrays. 
+It is a collection of nodes that are linked with each other. 
+A node contains two things first is data and second is a link that connects it with another node. 
+'''
